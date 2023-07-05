4fe45cf580a3358cc56f8c6dbe6fa9b13a76510b
Mateusz Głowiński <Mateusz Gowiski>
2023-07-05 12:28:53 +0200

    Add wait-for database

--- backend/persistence_layer/core.py
+++ backend/persistence_layer/core.py
@@ -4,7 +4,12 @@ from contextlib import asynccontextmanager
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = (
+    f"postgresql+asyncpg://"
+    f"{os.environ['DATABASE_USER']}:{os.environ['DATABASE_PASSWORD']}"
+    f"@{os.environ['DATABASE_HOST']}:{os.environ['DATABASE_PORT']}/"
+    f"{os.environ['DATABASE_NAME']}"
+)
 engine = create_async_engine(
     DATABASE_URL,
     future=True,
--- docker/development/backend/run-development.sh
+++ docker/development/backend/run-development.sh
@@ -5,6 +5,15 @@ set -e
 python -m pip install --upgrade --user --disable-pip-version-check pip
 pip install -r /requirements/development.txt
 
+# Wait for the database to be ready.
+/tools/wait-for "${DATABASE_HOST}":"${DATABASE_PORT}" -t 60
+if [ $? -ne 0 ]; then
+    echo Receipt of readiness from database failed. Exiting...
+    exit 1
+fi
+sleep 5 # Wait to be sure, that database is up.
+
+# Run database migrations.
 cd ./persistence_layer
 alembic upgrade head
 cd ../
